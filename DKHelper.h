aed63ef1eb84c108fb4ec006b72b00f12268b82c
kevindelordw <delord.kevin@gmail.com>
2014-04-21 15:41:10 +0200

    add predicate and improve nsobject defines

--- DKHelper.h
+++ DKHelper.h
@@ -6,7 +6,8 @@
 //  Copyright (c) 2014 smartmobilefactory. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#ifndef DKHelper_h__
+#define DKHelper_h__
 
 #pragma mark - NSObject+Block
 
@@ -113,6 +114,9 @@ CGRect CGRectSetOrigin(CGRect rect, CGPoint origin);
 
 #pragma mark - DKLog
 
+// Log
+#define __STRING_PRETTY_FUNCTION__                  [NSString stringWithFormat:@"%s", __PRETTY_FUNCTION__]
+
 #if defined (DEBUG)
 //# define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 # define DKLog(verbose, fmt, ...) if (verbose) { NSLog(fmt, ##__VA_ARGS__); }
@@ -132,19 +136,20 @@ CGRect CGRectSetOrigin(CGRect rect, CGPoint origin);
 
 // Numbers
 #pragma mark - Numbers
-#define MINMAX(v, min, max)                         ((v < min) ? min : (v > max) ? (max) : v )
+#define MINMAX(v, min, max)                         ((v min) ? min : (v > max) ? (max) : v )
 #define MONTH_IN_SECONDS                            (60 * 60 * 24 * 30)
 #define cL(v)                                       (long)(v)
 #define cUL(v)                                      (unsigned long)(v)
 
 // NSObjects
 #pragma mark - NSObjects
-#define OBJECT(dictionary, key)                     [dictionary objectForKey:key]
-#define VALID(dict, key)                            (dict && OBJECT(dict, key) && ![OBJECT(dict, key) isEqual:[NSNull null]])
-#define GET_NUMBER(dictionary, key)                 ( VALID(dictionary, key) ? [NSNumber numberWithInteger:[OBJECT(dictionary, key) integerValue]] : nil )
-#define GET_DATE(dictionary, key)                   ( VALID(dictionary, key) ? [NSDate dateFromString:OBJECT(dictionary, key)] : nil )
-#define GET_STRING(dictionary, key)                 ( VALID(dictionary, key) ? ( ![OBJECT(dictionary, key) isEqualToString:@""] ? OBJECT(dictionary, key) : nil ) : nil )
-#define GET_ASSET_URL(dictionary, key1, key2)       ( VALID(dictionary, key1) ? ( VALID(OBJECT(dictionary, key1), key2) ? OBJECT(OBJECT(dictionary, key1), key2) : nil ) : nil )
+#define OBJECT(dictionary, key)                     ( VALID(dictionary, key) ? [dictionary objectForKey:key] : nil )
+#define VALID(dict, key)                            (dict && [dict objectForKey:key] && ![[dict objectForKey:key] isEqual:[NSNull null]])
+#define GET_FLOAT(dictionary, key)                  ( VALID(dictionary, key) ? [NSNumber numberWithFloat:[[dictionary objectForKey:key] floatValue]] : nil )
+#define GET_NUMBER(dictionary, key)                 ( VALID(dictionary, key) ? [NSNumber numberWithInteger:[[dictionary objectForKey:key] integerValue]] : nil )
+#define GET_DATE(dictionary, key)                   ( VALID(dictionary, key) ? [NSDate dateFromString:[dictionary objectForKey:key]] : nil )
+#define GET_STRING(dictionary, key)                 ( VALID(dictionary, key) ? ( ![[dictionary objectForKey:key] isEqualToString:@""] ? [dictionary objectForKey:key] : nil ) : nil )
+#define GET_ASSET_URL(dictionary, key1, key2)       ( VALID(dictionary, key1) ? ( VALID([dictionary objectForKey:key1], key2) ? [[dictionary objectForKey:key1] objectForKey:key2] : nil ) : nil )
 
 // Screen
 #pragma mark - Screen
@@ -159,6 +164,10 @@ CGRect CGRectSetOrigin(CGRect rect, CGPoint origin);
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
 
+// Predicates
+#define OR_PREDICATE(a, b)                          (a ? [NSCompoundPredicate orPredicateWithSubpredicates:@[a, b]] : b)
+#define AND_PREDICATE(a, b)                         (a ? [NSCompoundPredicate andPredicateWithSubpredicates:@[a, b]] : b)
+
 // Common File Formats
 #define IMAGE_FILE_EXTENSIONS                       @[@".tif", @".tiff", @".gif", @".jpeg", @".jpg", @".png", @".pjpeg"]
 #define AUDIO_FILE_EXTENSIONS                       @[@".mp3", @".mp2", @".wma", @".aac", @".m4a", @".mp4", @".m4p"]
@@ -166,3 +175,5 @@ CGRect CGRectSetOrigin(CGRect rect, CGPoint origin);
 // Math
 #define degreesToRadians(degrees)                  ((degrees * M_PI) / 180.0)
 #define radiansToDegrees(radians)                  ((radians * 180.0) / M_PI)
+
+#endif
