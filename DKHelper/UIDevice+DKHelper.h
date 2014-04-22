beab5084ecde33ca1675f55a8fedbe3618d5cd6e
kevindelordw <delord.kevin@gmail.com>
2014-04-23 00:12:01 +0200

    add comments for UIDevice+DKHelper.h

--- DKHelper/UIDevice+DKHelper.h
+++ DKHelper/UIDevice+DKHelper.h
@@ -15,9 +15,35 @@
 
 #pragma mark - UIDevice+Model
 
+/**
+ * Returns the device name as a NSString.
+ *
+ * @discussion Example @"iPhone5,3" for @"iPhone 5c (GSM+CDMA)"
+ * @return The system name of the current device
+ */
 + (NSString *)platform;
+
+/**
+ * Returns a NSString describing the current device.
+ *
+ * @discussion Example @"iPhone 5c (GSM+CDMA)" for @"iPhone5,3"
+ * @return A string describing the current device with a user-friendly name.
+ */
 + (NSString *)platformString;
+
+/**
+ * Returns a Boolean value indicating whether the current device is a slow one or not.
+ *
+ * @discussion \niPod 3G, iPhone 4 and iPad 1 are considered as slow devices.
+ * @return YES if the current device is a slow one.
+ */
 + (BOOL)isSlowDevice;
+
+/**
+ * Returns a Boolean value indicating whether the current device is an iPad or not.
+ *
+ * @return YES if the current device is an iPad.
+ */
 + (BOOL)isIPad;
 
 @end
