50a917f17e5d144140746f35854766367ccfbf29
kevindelordw <delord.kevin@gmail.com>
2014-04-23 00:22:45 +0200

    add comments for NSObject+Block

--- DKHelper/NSObject+DKHelper.h
+++ DKHelper/NSObject+DKHelper.h
@@ -15,6 +15,12 @@
 
 #pragma mark - NSObject+Block
 
+/**
+ * Performs a code block after a given delay.
+ *
+ * @param block A block object containing the code to execute. This block takes no parameters and has no return value.
+ * @param delay The amount of time (measured in seconds) to wait before beginning the animations. Specify a value of 0 to begin the animations immediately.
+ */
 - (void)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay;
 
 @end
--- DKHelper/NSObject+DKHelper.m
+++ DKHelper/NSObject+DKHelper.m
@@ -13,8 +13,10 @@
 #pragma mark - NSObject+Block
 
 - (void)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay {
-    int64_t delta = (int64_t)(1.0e9 * delay);
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), block);
+    if (block != nil) {
+        int64_t delta = (int64_t)(1.0e9 * delay);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), block);
+    }
 }
 
 @end
