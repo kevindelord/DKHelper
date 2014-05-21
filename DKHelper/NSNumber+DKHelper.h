c375d69db6040f5b7e800ee51cf4cece057806ad
kevindelordw <delord.kevin@gmail.com>
2014-05-21 22:02:54 +0200

    comments the NSNumber class

--- DKHelper/NSNumber+DKHelper.h
+++ DKHelper/NSNumber+DKHelper.h
@@ -9,8 +9,18 @@
 #ifndef DKHelper_NSNumber_h__
 #define DKHelper_NSNumber_h__
 
+/**
+ * A categorized class of NSNumber to add a bunch of helping methods.
+ */
 @interface NSNumber (DKHelper)
 
+/**
+ * Returns the currency value as a string of the receiver.
+ *
+ * @discussion The locale is the default one.
+ *
+ * @return A NSString object representing the currency value of the receiver.
+ */
 - (NSString *)currencyStringValue;
 
 @end
