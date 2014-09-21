ad09877caf88ad73ef40503e5d634e20ade992c9
kevindelordw <delord.kevin@gmail.com>
2014-09-21 16:08:32 +0200

    ISO8601_DATE_FORMAT define has been removed. Use NSDate::ISO8601StringFormat instead

--- DKHelper/DKHelper.h
+++ DKHelper/DKHelper.h
@@ -86,7 +86,4 @@
 #define degreesToRadians(degrees)                  ((degrees * M_PI) / 180.0)
 #define radiansToDegrees(radians)                  ((radians * 180.0) / M_PI)
 
-// ISO Formats
-#define ISO8601_DATE_FORMAT                         @"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
-
 #endif
