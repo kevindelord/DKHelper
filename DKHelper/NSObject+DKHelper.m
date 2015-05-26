aff5d0ab992a5f6a2897439ce77721097d9c7a1e
kevindelord <delord.kevin@gmail.com>
2015-05-26 11:20:54 +0200

    Add deprecated perform block methods.

--- DKHelper/NSObject+DKHelper.m
+++ DKHelper/NSObject+DKHelper.m
@@ -49,4 +49,23 @@
     });
 }
 
+#pragma mark - Deprecated
+
+- (void)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay completion:(void (^)(void))completionBlock {
+    if (block != nil) {
+        int64_t delta = (int64_t)(1.0e9 * delay);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), ^(void){
+            block();
+            completionBlock();
+        });
+    }
+}
+
+- (void)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay {
+    if (block != nil) {
+        int64_t delta = (int64_t)(1.0e9 * delay);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), block);
+    }
+}
+
 @end
