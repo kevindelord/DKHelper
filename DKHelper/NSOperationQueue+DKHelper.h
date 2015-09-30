b9e7b2d6ad082d929f93b95bea97307bae86f1da
Pierre Rothmaler <proth@users.noreply.github.com>
2015-09-30 12:35:38 +0200

    Update NSOperationQueue+DKHelper.h

--- DKHelper/NSOperationQueue+DKHelper.h
+++ DKHelper/NSOperationQueue+DKHelper.h
@@ -13,6 +13,8 @@
 /**
  * Wraps the specified block in an operation object and adds it to the receiver. If the given time passed the operation will get cancelled and the timeoutBlock gets called.
  *
+ * @discussion make sure to check whether the operation is cancelled in your execution block
+ *
  * @param block The block to execute from the operation object.
  * @param timeout Timeout after that the operation should be cancelled
  * @param timeoutBlock The block that gets called after the operation timed out
