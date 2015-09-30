321333116ed650f54ec09bc1486597e3afef0c30
Pierre Rothmaler <me@pierre.me>
2015-09-30 12:15:17 +0200

    added NSOperationQueue+Timeout

---
+++ DKHelper/NSOperationQueue+Timeout.h
@@ -0,0 +1,15 @@
+//
+//  NSOperationQueue+Timeout.h
+//  SchneiderIntercom
+//
+//  Created by Pierre Rothmaler on 30/09/15.
+//  Copyright (c) 2015 Smart Mobile Factory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSOperationQueue (Timeout)
+
+- (NSOperation *)addOperationWithBlock:(void (^)(NSOperation *operation))block timeout:(NSTimeInterval)timeout timeoutBlock:(void (^)(void))timeoutBlock;
+
+@end
---
+++ DKHelper/NSOperationQueue+Timeout.m
@@ -0,0 +1,35 @@
+//
+//  NSOperationQueue+Timeout.m
+//
+//
+//  Created by Pierre Rothmaler on 30/09/15.
+//  Copyright (c) 2015 Smart Mobile Factory. All rights reserved.
+//
+
+#import "NSOperationQueue+Timeout.h"
+
+@implementation NSOperationQueue (Timeout)
+
+- (NSOperation *)addOperationWithBlock:(void (^)(NSOperation *operation))block timeout:(NSTimeInterval)timeout timeoutBlock:(void (^)(void))timeoutBlock {
+	NSBlockOperation *blockOperation = [[NSBlockOperation alloc] init];
+	NSBlockOperation __weak *weakOperation = blockOperation;
+
+	[blockOperation addExecutionBlock:^{
+		block(weakOperation);
+	}];
+
+	[self addOperation:blockOperation];
+
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(timeout * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		if (weakOperation && ![weakOperation isFinished]) {
+			[weakOperation cancel];
+			if (timeoutBlock) {
+				timeoutBlock();
+			}
+		}
+	});
+
+	return blockOperation;
+}
+
+@end
