6156c180dc6907f23598b0fd7dc41b82520a02a9
kevindelord <delord.kevin@gmail.com>
2015-10-29 16:37:02 +0100

    NSObject: fix crashes when block parameters were nil

--- DKHelper/NSObject+DKHelper.m
+++ DKHelper/NSObject+DKHelper.m
@@ -13,40 +13,52 @@
 #pragma mark - NSObject+Block
 
 - (void)performBlock:(void (^)(void))block completion:(void (^)(void))completionBlock {
-    if (block != nil) {
-        block();
-        completionBlock();
-    }
+	if (block != nil) {
+		block();
+		if (completionBlock != nil) {
+			completionBlock();
+		}
+	}
 }
 
 - (void)performBlockAfterDelay:(NSTimeInterval)delay block:(void (^)(void))block completion:(void (^)(void))completionBlock {
-    if (block != nil) {
-        int64_t delta = (int64_t)(1.0e9 * delay);
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), ^(void){
-            block();
-            completionBlock();
-        });
-    }
+	if (block != nil) {
+		int64_t delta = (int64_t)(1.0e9 * delay);
+		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), ^(void){
+			block();
+
+			if (completionBlock != nil) {
+				completionBlock();
+			}
+		});
+	}
 }
 
 - (void)performBlockAfterDelay:(NSTimeInterval)delay block:(void (^)(void))block {
-    if (block != nil) {
-        int64_t delta = (int64_t)(1.0e9 * delay);
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), block);
-    }
+	if (block != nil) {
+		int64_t delta = (int64_t)(1.0e9 * delay);
+		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), block);
+	}
 }
 
 - (void)performBlockInBackground:(void (^)(void))block {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, (unsigned long)NULL), ^(void) {
-        block();
-    });
+	if (block != nil) {
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, (unsigned long)NULL), ^(void) {
+			block();
+		});
+	}
 }
 
 - (void)performBlockInBackground:(void (^)(void))block completion:(void (^)(void))completionBlock {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, (unsigned long)NULL), ^(void) {
-        block();
-        dispatch_async(dispatch_get_main_queue(), completionBlock);
-    });
+	if (block != nil) {
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, (unsigned long)NULL), ^(void) {
+			block();
+
+			if (completionBlock != nil) {
+				dispatch_async(dispatch_get_main_queue(), completionBlock);
+			}
+		});
+	}
 }
 
 @end
