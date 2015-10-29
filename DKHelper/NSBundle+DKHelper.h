2c406775f6f0a713b8cb4194f59705e50008b914
kevindelord <delord.kevin@gmail.com>
2015-10-29 16:31:30 +0100

    NSBundle: add getter methods

--- DKHelper/NSBundle+DKHelper.h
+++ DKHelper/NSBundle+DKHelper.h
@@ -11,7 +11,7 @@
 @interface NSBundle (DKHelper)
 
 /**
- * Returns the value associated with the specified key in the receiver's information property list.
+ * Returns the value related to the given key in the receiver's information property list.
  *
  * @discussion This function will raise an exception if no value for the given key has been found.
  * Use this function to get static/setup data.
@@ -20,5 +20,38 @@
  * @return Returns the value associated with key in the receiver's property list (Info.plist). The localized value of a key is returned when one is available.
  */
 + (id)entryInPListForKey:(NSString *)key;
+
+/**
+ * Returns the value related to the given key in the receiver's information property list.
+ *
+ * @discussion This function will raise an exception if no value for the given key has been found.
+ * Use this function to get static/setup data.
+ *
+ * @param key A key in the receiver's property list.
+ * @return Returns a NSString value associated with key in the receiver's property list (Info.plist). The localized value of a key is returned when one is available.
+ */
++ (NSString *)stringEntryInPListForKey:(NSString *)key;
+
+/**
+ * Returns the value related to the given key in the receiver's information property list.
+ *
+ * @discussion This function will raise an exception if no value for the given key has been found.
+ * Use this function to get static/setup data.
+ *
+ * @param key A key in the receiver's property list.
+ * @return Returns a BOOL value associated with key in the receiver's property list (Info.plist). The localized value of a key is returned when one is available.
+ */
++ (BOOL)booleanEntryInPListForKey:(NSString *)key;
+
+/**
+ * Returns the value related to the given key in the receiver's information property list.
+ *
+ * @discussion This function will raise an exception if no value for the given key has been found.
+ * Use this function to get static/setup data.
+ *
+ * @param key A key in the receiver's property list.
+ * @return Returns a NSNumber value associated with key in the receiver's property list (Info.plist). The localized value of a key is returned when one is available.
+ */
++ (NSNumber *)numberEntryInPListForKey:(NSString *)key;
 
 @end
