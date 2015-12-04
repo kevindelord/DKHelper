b180e38b76d948dd6c4046271260ba8ebd63eed0
konDeichmann <k.deichmann@mac.com>
2015-12-04 14:55:06 +0100

    adding test for Function date Strings

--- Example/DKHelperTests/NSDateTests.swift
+++ Example/DKHelperTests/NSDateTests.swift
@@ -91,3 +91,54 @@ extension NSDateTests {
 		}
 	}
 }
+
+// MARK: - Create date from a String
+
+extension NSDateTests {
+
+	func testShouldReturnValidAndCorrectDate() {
+		let date = NSDate(fromString: "Dec 24, 1992", style: .MediumStyle)
+		XCTAssertNotNil(date)
+		XCTAssertEqual(date?.year(), 1992)
+		XCTAssertEqual(date?.day(), 24)
+		XCTAssertEqual(date?.month(), 12)
+	}
+
+	func testShouldReturnNilDate() {
+		let date = NSDate(fromString: "Dec 24, 1992", style: .FullStyle)
+		XCTAssertNil(date)
+	}
+
+	func testShouldReturnValidAndCorrectDateWithStringFormat() {
+		let date = NSDate(fromString: "24.12.1992", format: "dd.MM.yyyy")
+		XCTAssertNotNil(date)
+		XCTAssertEqual(date?.year(), 1992)
+		XCTAssertEqual(date?.day(), 24)
+		XCTAssertEqual(date?.month(), 12)
+	}
+
+	func testShouldReturnValidTodayDate() {
+		if let today = self.today {
+			let date = NSDate(fromISOString: today.ISO8601StringValue())
+			XCTAssertNotNil(date)
+			XCTAssertEqual(date?.year(), today.year())
+			XCTAssertEqual(date?.day(), today.day())
+			XCTAssertEqual(date?.month(), today.month())
+			XCTAssertEqual(date?.hour(), today.hour())
+			XCTAssertEqual(date?.minute(), today.minute())
+			XCTAssertEqual(date?.second(), today.second())
+		} else {
+			XCTFail()
+		}
+	}
+
+	func testReturnValidStringValueOfTodayDate() {
+		if let today = self.today {
+			let value = today.stringValue()
+			XCTAssertNotNil(value)
+			XCTAssertTrue(value.characters.count > 0)
+		} else {
+			XCTFail()
+		}
+	}
+}
